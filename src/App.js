3e1744c9d01fb898b4e156ccc80bd626fbd7c1a6
Zevi Liao <jointjm@gmail.com>
2019-06-02 15:57:02 +0800

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { Component } from 'react'
 import './App.css';
+import axios from 'axios';
+// import fakeAxios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = 'https://jsonplaceholder.typicode.com/comments';
+
+// mock.onGet(/\/comments/).reply(200,
+//   [
+//     { id: 1, name: 'John Smith' },
+//     { id: 2, name: 'John Doe' }
+//   ]
+// )
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      listData: []
+    }
+  }
+
+  mockData = async (uid) => { // await
+    try {
+      let rsp = await axios.get(API)
+      this.setState({
+        listData: rsp.data,
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  urlData = () => {
+    axios.get(API) // external data
+      .then(result => this.setState({
+        listData: result.data,
+      }))
+      .catch(error =>
+        console.log(error)
+      );
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <button onClick={this.mockData}>  hijacked by mocker </button>
+        <button onClick={this.urlData}>  fecth data from extranal url </button>
+<ul>
+        {
+          this.state.listData.filter(o => o.id < 10).map(o => <li key="id"> {o.name} </li>)
+        }
+        </ul>        
+      </div>
+    )
+  }
 }
-
-export default App;
