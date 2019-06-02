1a8f803260bc2072f1cfb2c035c09b8a74c91128
Zevi Liao <jointjm@gmail.com>
2019-06-02 14:36:27 +0800

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { Component } from 'react'
 import './App.scss';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
+const API = 'https://jsonplaceholder.typicode.com/comments/';
 
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      listData: []
+    }
+  }
 
-    </div>
-  );
+  mockData = () => {
+    // axios.get(API)
+    //   .then(result => this.setState({
+    //     listData: result.data,
+    //     isLoading: false
+    //   }))
+    //   .catch(error => this.setState({
+    //     error,
+    //     isLoading: false
+    //   }));
+  }
+
+  urlData = () => {
+    axios.get(API)
+      .then(result => this.setState({
+        listData: result.data,
+      }))
+      .catch(error =>
+        console.log(error)
+      );
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <button onClick={this.mockData}>  hijacked by mocker </button>
+        <button onClick={this.urlData}>  fecth data from extranal url </button>
+<ul>
+        {
+          this.state.listData.filter(o => o.id < 10).map(o => <li key="id"> {o.name} </li>)
+        }
+        </ul>  
+      </div>
+    )
+  }
 }
-
-export default App;
