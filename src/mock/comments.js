52a6645a474b969a12245a8deef287477145faf9
Zevi Liao <jointjm@gmail.com>
2019-06-02 19:26:53 +0800

    by pass request if not matched.

--- src/mock/comments.js
+++ src/mock/comments.js
@@ -1,12 +1,17 @@
 
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter'
-let mock = new MockAdapter(axios)
-mock.onGet(/\/comments\/fake/).reply(200,
-    [
-        { id: 1, name: 'John Smith' },
-        { id: 2, name: 'John Doe' }
-    ]
-)
+let mock = new MockAdapter(axios, { delayResponse: 500 })
+mock
+    .onGet(/\/comments\/fake/).reply(
+        (config) => {
+            return [200, [
+                { id: 1, name: 'John Smith' },
+                { id: 2, name: 'John Doe' }
+            ]]
+        })
+    .onAny().passThrough();
 
-export default axios
+export {
+    axios
+}
